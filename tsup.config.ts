a5cb90d345a45ebbdcb9f834355486050769e9c3
Avirup Ghosh <oviirup@gmail.com>
2025-04-15 14:32:59 +0530

    🐛 fix tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,34 +6,27 @@ import pkg from './package.json';
 
 const patterns = Object.keys(pkg.exports).flatMap((entry) => {
   // add extensions to path glob
-  return path.parse(entry).ext ? entry : [`${entry}.{js,ts,css}`, `${entry}/index.{js,ts,css}`];
+  entry = entry.replace(/^\./, './src');
+  return [`${entry}.{js,ts,css}`, `${entry}/index.{js,ts}`];
 });
 
-const srcPath = path.join(__dirname, 'src');
-
-type Entry = Record<string, string>;
-
-const entries = fs
-  .readdirSync(srcPath, { recursive: true, withFileTypes: true })
+const files = fs
+  .readdirSync('src', { recursive: true, withFileTypes: true })
   // filter out directories
   .filter((entry) => entry.isFile())
   // get filepath relative to src path
-  .map((entry) => path.relative(srcPath, path.join(entry.parentPath, entry.name)).replace(/\\/g, '/'))
-  // loop through entries and append matched entries
-  .reduce<Entry>((acc, filePath) => {
-    if (match([filePath], patterns)[0]) {
-      const { name, dir } = path.parse(filePath);
-      const outPath = path.join(dir, name);
-      acc[outPath] = path.join('src', filePath);
-    }
-    return acc;
-  }, {});
+  .map((entry) => {
+    const fullPath = path.join(entry.parentPath, entry.name);
+    return path.relative(__dirname, fullPath).replace(/\\/g, '/');
+  });
+
+const entries = match(files, patterns);
 
 export default defineConfig((cfg) => ({
   ...cfg,
   entry: entries,
-  outDir: 'dist',
   splitting: true,
   clean: true,
-  dts: true,
+  dts: { entry: entries.filter((e) => !e.endsWith('.css')) },
+  minify: !cfg.watch,
 }));
